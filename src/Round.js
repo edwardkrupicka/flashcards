894c717c4a6379afe8cbdfba52aac91682f9263a
edward krupicka <edwardkrupicka@gmail.com>
2021-10-14 19:35:04 -0600

    Add Round.js files and add functions to pass test

---
+++ src/Round.js
@@ -0,0 +1,34 @@
+const Turn = require("./Turn");
+
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards;
+    this.currentCard = this.deck[0];
+    this.incorrectGuesses = [];
+    this.turns = 0;
+  }
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+  takeTurn(userGuess) {
+    let newTurn = new Turn(userGuess, this.currentCard);
+    if (!newTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(newTurn.card.id)
+    }
+    this.turns++;
+    this.currentCard = this.deck[this.turns];
+    if (this.turns > this.deck.length) {
+      this.endRound();
+    }
+    return newTurn.giveFeedback();
+  }
+  calculatePercentCorrect() {
+    return Math.floor(((this.deck.length - this.incorrectGuesses.length) / this.turns) * 100);
+  }
+  endRound() {
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
+  }
+
+};
+
+module.exports = Round;
